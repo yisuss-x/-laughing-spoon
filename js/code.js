fd7b6599d1cd871e480783763746e9b8331296ac
Jesus <jesusherreraeo@gmail.com>
2022-12-13 21:36:01 -0300

    termina de hacer correcciones

--- js/code.js
+++ js/code.js
@@ -7,6 +7,10 @@ let caf = document.querySelector(".caf");
 let conmebol = document.querySelector(".conmebol");
 let uefa = document.querySelector(".uefa");
 let ofc = document.querySelector(".ofc");
+let facebook = document.querySelector(".fa-facebook-square")
+let instagram = document.querySelector(".fa-instagram-square")
+let youtube = document.querySelector(".fa-youtube-square")
+let twitter = document.querySelector(".fa-twitter-square")
 
 afa.addEventListener("click", () => {
     afa = confirm("Estas por salir de la pagina. Estas ¿seguro?");
@@ -76,6 +80,42 @@ ofc.addEventListener("click", () => {
 
     if (ofc == true){
         location.href = "https://www.oceaniafootball.com/";
+    } else {
+        alert("Por favor decida si ir o quedarse");
+    }
+});
+facebook.addEventListener("click", () => {
+    facebook = confirm("Estas por salir de la pagina. Estas ¿seguro?");
+
+    if (facebook == true){
+        location.href = "https://www.facebook.com/";
+    } else {
+        alert("Por favor decida si ir o quedarse");
+    }
+});
+instagram.addEventListener("click", () => {
+    instagram = confirm("Estas por salir de la pagina. Estas ¿seguro?");
+
+    if (instagram == true){
+        location.href = "https://www.instagram.com/";
+    } else {
+        alert("Por favor decida si ir o quedarse");
+    }
+});
+youtube.addEventListener("click", () => {
+    youtube = confirm("Estas por salir de la pagina. Estas ¿seguro?");
+
+    if (youtube == true){
+        location.href = "https://www.youtube.com/";
+    } else {
+        alert("Por favor decida si ir o quedarse");
+    }
+});
+twitter.addEventListener("click", () => {
+    twitter = confirm("Estas por salir de la pagina. Estas ¿seguro?");
+
+    if (twitter == true){
+        location.href = "https://twitter.com/?lang=es";
     } else {
         alert("Por favor decida si ir o quedarse");
     }
